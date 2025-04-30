4a92dce43f002415ffc0514476fd7de60da21354
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-30 19:43:24 -0300

    componente input e pagina de login

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,8 +1,29 @@
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { Container } from '../../container';
+import { Input } from '../../components/input';
+
 export default function Login() {
     return (
-      <div>
-        <h1>Pagina login</h1>
-      </div>
+      <Container>
+        <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+          <Link to='/' className='mb-6 max-w-sm w-full'>
+            <img 
+              src={logoImg} 
+              alt="Logo do site" 
+              className='w-full'
+              />
+          </Link>
+          
+          <form className='bg-white max-w-xl w-full rounded-lg'>
+            <Input 
+              type= "email"
+              placeholder= "digite seu email"
+              name= "email"
+            />
+          </form>
+        </div>
+      </Container>
     )
 }
   
