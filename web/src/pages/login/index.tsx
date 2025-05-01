700c078e5d1b7b3012a7eb9d38f9e20b44d9cf7e
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-05-01 08:30:57 -0300

    Complementação da página de login

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -2,28 +2,60 @@ import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { Container } from '../../container';
 import { Input } from '../../components/input';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  email: z.string().email("Insira um email válido").nonempty("O campo emial é obrigatório."),
+  password: z.string().nonempty("O campo senha é obirgatório.")
+});
+
+type FormData = z.infer<typeof schema>
 
 export default function Login() {
-    return (
-      <Container>
-        <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
-          <Link to='/' className='mb-6 max-w-sm w-full'>
-            <img 
-              src={logoImg} 
-              alt="Logo do site" 
-              className='w-full'
-              />
-          </Link>
-          
-          <form className='bg-white max-w-xl w-full rounded-lg'>
-            <Input 
-              type= "email"
-              placeholder= "digite seu email"
-              name= "email"
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+        <Link to='/' className='mb-6 max-w-sm w-full'>
+          <img 
+            src={logoImg} 
+            alt="Logo do site" 
+            className='w-full'
             />
-          </form>
-        </div>
-      </Container>
-    )
+        </Link>
+        
+        <form className='bg-white max-w-xl w-full rounded-lg' onSubmit={(handleSubmit(onSubmit))}>
+          <div className='mb-3'>
+            <Input 
+              type="email"
+              placeholder="digite seu email"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className='mb-3'>
+            <Input 
+              type="password"
+              placeholder="digite sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button>Acessar</button>
+        </form>
+      </div>
+    </Container>
+  )
 }
   
