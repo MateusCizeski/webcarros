5aab88af6c6c199919ef0a7d7b4c077f1986a940
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-05-22 19:31:20 -0300

    método de deletar carro na dashboard page

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -2,9 +2,17 @@ import { useEffect, useState, useContext } from 'react';
 import { Container } from '../../components/container';
 import { DashboardHeader } from '../../components/panelHeader';
 import { FiTrash2 } from 'react-icons/fi';
-import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
-import { db } from '../../services/firebaseConnection';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db, storage } from '../../services/firebaseConnection';
 import { AuthContext } from '../../context/AuthContext';
+import { deleteObject, ref } from 'firebase/storage';
 
 interface CarsProps {
   id: string;
@@ -57,32 +65,56 @@ export default function Dashboard() {
     loadCars();
   }, [user]);
 
+  async function handleDeleteCar(car: CarsProps) {
+    const docRef = doc(db, 'cars', car.id);
+    await deleteDoc(docRef);
+
+    car.images.map(async (image) => {
+      const imagePath = `images/${image.uid}/${image.name}`;
+      const imageRef = ref(storage, imagePath);
+
+      try {
+        await deleteObject(imageRef);
+        setCars(cars.filter((car) => car.id !== car.id));
+      } catch (err) {
+        console.log('Erro ao excluir essa imagem');
+      }
+    });
+  }
+
   return (
     <Container>
       <DashboardHeader />
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg relative">
-          <button
-            onClick={() => {}}
-            className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2"
-          >
-            <FiTrash2 size={26} color="#000" />
-          </button>
-          <img className="w-full rounded-lg mb-2 max-h-70" src="" />
-          <p className="font-bold mt-1 px-2 mb-2"></p>
+        {cars.map((car) => (
+          <section key={car.id} className="w-full bg-white rounded-lg relative">
+            <button
+              onClick={() => handleDeleteCar(car)}
+              className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2"
+            >
+              <FiTrash2 size={26} color="#000" />
+            </button>
+            <img
+              className="w-full rounded-lg mb-2 max-h-70"
+              src={car.images[0].url}
+            />
+            <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700"></span>
-            <strong className="text-black font-bold mt-4"></strong>
-          </div>
-
-          <div className="w-full h-px bg-slate-200 my-2">
-            <div className="px-2 pb-2">
-              <span className="text-black"></span>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700">
+                Ano {car.year}| {car.km} KM
+              </span>
+              <strong className="text-black font-bold mt-4">{car.price}</strong>
             </div>
-          </div>
-        </section>
+
+            <div className="w-full h-px bg-slate-200 my-2">
+              <div className="px-2 pb-2">
+                <span className="text-black">{car.city}</span>
+              </div>
+            </div>
+          </section>
+        ))}
       </main>
     </Container>
   );
