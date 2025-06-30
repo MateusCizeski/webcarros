a0da70cae2f19574d762534de37e86e744256abd
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-06-30 20:23:52 -0300

    Estrutura de DTOs e alteração para o jwt

---
+++ backend/CarPlataform/DTOs/CarCreateDto.cs
@@ -0,0 +1,25 @@
+﻿namespace CarPlataform.DTOs
+{
+    public class CarCreateDto
+    {
+        public string Name { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Year { get; set; } = null!;
+        public string Km { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Created { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Owner { get; set; } = null!;
+        public string Uid { get; set; } = null!;
+        public string Whatsapp { get; set; } = null!;
+        public List<CarImageDto> Images { get; set; } = new();
+    }
+
+    public class CarImageDto
+    {
+        public string Name { get; set; } = null!;
+        public string Url { get; set; } = null!;
+        public string Uid { get; set; } = null!;
+    }
+}
--- backend/CarPlataform/Program.cs
+++ backend/CarPlataform/Program.cs
@@ -1,15 +1,35 @@
+using CarPlataform.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt => {
+        opt.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            ),
+            ValidateIssuerSigningKey = true
+        };
+    });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
